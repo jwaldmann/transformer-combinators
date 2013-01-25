ac4c0f934340108bf4dfe858d43d8a64a8dafe54
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2013-01-25 14:19:29 +0100

    ansatz

--- Control/Concurrent/Combine.hs
+++ Control/Concurrent/Combine.hs
@@ -2,9 +2,40 @@ module Control.Concurrent.Combine where
 
 import Control.Concurrent.Combine.Transformer 
 
-import Control.Monad ( guard )
+import Control.Concurrent.Async
+import Control.Monad ( forM )
 
-assert :: (a1 -> Bool) -> Transformer a1 a1 b b
-assert pred = transform
-    ( \ x -> do guard $ pred x ; return x ) id
+andthen s t = transform
+    ( inside s `bind` inside t )
+    ( outside t `bind` outside s )
 
+orelse s t = transform 
+    ( \ x -> do
+        my <- inside s x
+        case my of
+            Just y -> return $ Just $ Left y
+            Nothing -> efmap Right (inside t) x
+    ) ( \ e -> case e of
+        Left y -> outside s y
+        Right z -> outside t z
+    )
+
+parallel ts = transform
+    ( \ x -> runAnyJust $ map ( \ t -> inside t x ) ts)
+    ( \ (k,y) -> outside (ts !! k) y )
+
+runAnyJust actions = do
+    as <- forM (zip [0 :: Int .. ] actions) 
+        $ \ (k,action) -> 
+           async $ do r <- action ; return (k, r)
+    let waiter 0 = return Nothing
+        waiter n = do
+            (_ , (k,r)) <- waitAny as
+            case r of
+                Nothing -> waiter $ n - 1
+                Just x  -> do
+                    forM as cancel
+                    return $ Just (k,x)
+    waiter $ length actions 
+
+        
--- Control/Concurrent/Combine/Transformer.hs
+++ Control/Concurrent/Combine/Transformer.hs
@@ -1,22 +1,35 @@
+{-# language ExistentialQuantification #-}
+
 module Control.Concurrent.Combine.Transformer where
 
-import Control.Concurrent.Combine.Action (Action)
-import qualified Control.Concurrent.Combine.Action as A
+import Control.Monad ( guard, when )
 
-newtype Transformer a a' b' b =
-    Transformer ( Action a' b' -> Action a b )
+type Action a b = a -> IO (Maybe b) 
 
-fail = Transformer $  \ _ -> A.fail
+bind f g = \ x -> do
+    my <- f x
+    case my of
+        Nothing -> return Nothing
+        Just y -> g y
 
-apply :: Transformer a a' b' b
-      -> Action a' b'
-      -> Action a b
-apply (Transformer t) a = t a
+efmap f a = \ x -> fmap (fmap f) (a x)
 
-run :: Transformer a b' b' b -> a -> IO (Maybe b)
-run t = A.execute (apply t $ A.succeed id)
+lift f = return . return . f
 
-transform fore back = Transformer $ \ act -> 
-    A.action $ \ input -> case fore input of
-        Just input' -> fmap back $ A.execute act input'
-        Nothing     -> return Nothing
+data Transformer a a' b' b = 
+    Transformer { inside :: Action a a'
+                , outside :: Action b' b 
+                }
+
+transform i o = 
+    Transformer { inside = i, outside = o }
+
+run :: Transformer a1 b' b' a -> a1 -> IO (Maybe a)
+run t = inside t `bind` outside t 
+
+assert :: (a' -> Bool) -> Transformer a' a' b b
+assert pred = transform
+    ( \ x -> return $ guard (pred x) >> return x ) 
+    ( lift id )
+
+fail = assert (const False)
